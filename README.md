f9d0e7d2426096dabee4716de9b332cb08689b7f
Nishii Takahiro <n.suudai@gmail.com>
2022-02-16 14:29:24 +0900

    [Linux] 必要なサードパーティライブラリをインストールするスクリプトを追加

--- README.md
+++ README.md
@@ -10,12 +10,23 @@
 - Install Visual Studio 2022
 - Install [Git](https://git-scm.com/)
 
+## Linux
+```sh
+$ [sudo] apt-get install build-essential autoconf libtool pkg-config
+$ [sudo] apt-get install cmake
+$ [sudo] apt-get install libx11-dev
+```
+
 # Clone the repository
 Before building, you need to clone the SUU-Studio github repository. Use following commands to clone the SUU-Studio repository.
 
 ## Windows
 ```cmd
 > git clone https://github.com/SUU-Lab/SUU-Studio.git
+```
+## Linux
+```sh
+$ git clone https://github.com/SUU-Lab/SUU-Studio.git
 ```
 
 # Build and install dependencies
@@ -25,4 +36,9 @@ After clone, build and install third-party libaries and tools for SUU-Studio's d
 ```cmd
 > cd SUU-Studio
 > Setup.bat
+```
+## Linux
+```sh
+$ cd SUU-Studio
+$ ./setup-linux.sh
 ```
--- ThirdParty/install-all-linux.sh
+++ ThirdParty/install-all-linux.sh
@@ -4,29 +4,29 @@
 CURRENT=$(cd $(dirname $0);pwd)
 pushd $CURRENT
 
+INSTALL_DIR=Install/Linux
+
 # Install pre-requisites
-if [ ! -d "Install" ]; then
-    mkdir Install
+if [ ! -d "$INSTALL_DIR" ]; then
+    mkdir -p $INSTALL_DIR
 fi
 
-pushd Install
+pushd Install/Linux
 
 echo ---------- CMake ----------
 CMAKE_VERSION=3.22.2
 CMAKE_ARCHIVE_NAME=cmake-$CMAKE_VERSION-linux-x86_64
-CMAKE_PATH=$CURRENT/Install/$CMAKE_ARCHIVE_NAME/bin
+CMAKE_PATH=$CURRENT/$INSTALL_DIR/$CMAKE_ARCHIVE_NAME/bin
 CMAKE_PATH_NAME="$CMAKE_PATH"
 if [ ! -d $CMAKE_PATH_NAME ]; then
-    echo "$CMAKE_PATH_NAME is not exist."
     wget -q https://github.com/Kitware/CMake/releases/download/v$CMAKE_VERSION/$CMAKE_ARCHIVE_NAME.tar.gz
     tar zxf $CMAKE_ARCHIVE_NAME.tar.gz
     rm -rf $CMAKE_ARCHIVE_NAME.tar.gz
 fi
 
-export CMAKE_PATH
 $CMAKE_PATH/cmake --version
 
-# pop Install
+# pop $INSTALL_DIR
 popd
 
 # Clone and build
@@ -46,7 +46,7 @@ fi
 pushd grpc
 
 BUILD_DIR=.build_Linux
-INSTALL_DIR=$CURRENT/../Install/Linux/grpc-$GRPC_VERSION
+GRPC_INSTALL_DIR=$CURRENT/$INSTALL_DIR/grpc-$GRPC_VERSION
 
 if [ ! -d "$BUILD_DIR" ]; then
     mkdir $BUILD_DIR
@@ -55,21 +55,14 @@ fi
 $CMAKE_PATH/cmake \
 -DgRPC_INSTALL=ON \
 -DgRPC_BUILD_TESTS=OFF \
--DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
+-DCMAKE_INSTALL_PREFIX=$GRPC_INSTALL_DIR \
 -DCMAKE_BUILD_TYPE=Release \
 -B$BUILD_DIR
 
-if [ ! -d "$BUILD_DIR/Release" ]; then
-    pushd $BUILD_DIR
-    make
-    popd
-fi
-
-if [ ! -d "$INSTALL_DIR" ]; then
-    pushd $BUILD_DIR
-    make install
-    popd
-fi
+pushd $BUILD_DIR
+make
+make install
+popd
 
 # pop grpc
 popd
